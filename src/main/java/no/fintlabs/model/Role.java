c14c623324c9581a4c3228bf2569f4ad356ade03
Bernt Andreas Johnsen <berntj@viken.no>
2022-12-13 15:39:27 +0100

    WIP - entityTorole messages is saved to role catalog

--- src/main/java/no/fintlabs/model/Role.java
+++ src/main/java/no/fintlabs/model/Role.java
@@ -2,6 +2,7 @@ package no.fintlabs.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.AccessLevel;
@@ -12,16 +13,16 @@ import lombok.extern.slf4j.Slf4j;
 @Data
 @Slf4j
 @Entity
+@Table(name="Roles")
 @AllArgsConstructor
 @NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
 public class Role {
     @Id
-    private String roleid;
-    private String resourceid;
-    private String rolename;
-    private String roletype;
-    private String rolesubtype;
-    private boolean aggregatedrole;
-    private String rolesource;
-
+    private String resourceId;
+    private String roleId;
+    private String roleName;
+    private String roleType;
+    private String roleSubType;
+    private boolean aggregatedRole;
+    private String roleSource;
 }
