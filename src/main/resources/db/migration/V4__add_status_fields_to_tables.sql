553300f48618dcc7a9a70dc0ee2d554b35177ccd
berntjoh <berntj@viken.no>
2024-08-13 09:51:17 +0200

    FKS-848: removed duplicate db migration file

--- src/main/resources/db/migration/V4__add_status_fields_to_tables.sql
+++
@@ -1,4 +0,0 @@
-alter table roles add role_status varchar(255);
-alter table roles add role_status_changed timestamp;
-alter table role_memberships add membership_status varchar(255);
-alter table role_memberships add membership_status_changed timestamp;
