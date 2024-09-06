e2e1d348b4905acad04e0e052c5233b72e3e41c1
Morten Solberg <morten@source-code.no>
2024-09-06 14:20:35 +0200

    FKS-924: Adjust logging

--- src/main/java/no/fintlabs/role/RoleConsumerConfiguration.java
+++ src/main/java/no/fintlabs/role/RoleConsumerConfiguration.java
@@ -4,14 +4,19 @@ import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.cache.FintCache;
 import no.fintlabs.kafka.entity.EntityConsumerFactoryService;
 import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters;
+import no.fintlabs.member.Member;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Slf4j
 @Configuration
 public class RoleConsumerConfiguration {
+
     private final RoleService roleService;
 
     public RoleConsumerConfiguration(RoleService roleService) {
@@ -21,7 +26,7 @@ public class RoleConsumerConfiguration {
     @Bean
     public ConcurrentMessageListenerContainer<String, Role> roleConsumer(
             EntityConsumerFactoryService entityConsumerFactoryService
-    ){
+    ) {
         EntityTopicNameParameters entityTopicNameParameters = EntityTopicNameParameters
                 .builder()
                 .resource("role")
@@ -29,14 +34,18 @@ public class RoleConsumerConfiguration {
 
         return entityConsumerFactoryService.createFactory(
                         Role.class,
-                        (ConsumerRecord<String,Role> consumerRecord) -> {
-                            log.info("Role consumed from Kafka with roleid: {}, members: {}, resourceid: {}"
-                                    ,consumerRecord.value().getRoleId(), consumerRecord.value().getMembers(), consumerRecord.value().getResourceId());
+                        (ConsumerRecord<String, Role> consumerRecord) -> {
+                            List<Long> memberIds = consumerRecord.value().getMembers().stream().map(Member::getId).collect(Collectors.toList());
 
-                            roleService.save(consumerRecord.value());
+                            log.info("Role consumed from Kafka with roleid: {}, members: {}, resourceid: {}"
+                                    , consumerRecord.value().getRoleId(), memberIds, consumerRecord.value().getResourceId());
+
+                            Role savedRole = roleService.save(consumerRecord.value());
+
+                            List<Long> savedMemberIds = savedRole.getMembers().stream().map(Member::getId).collect(Collectors.toList());
 
                             log.info("Role saved to database with roleid: {}, members: {}, resourceid: {}"
-                                    ,consumerRecord.value().getRoleId(), consumerRecord.value().getMembers(), consumerRecord.value().getResourceId());
+                                    , savedRole.getRoleId(), savedMemberIds, savedRole.getResourceId());
                         }
                 )
                 .createContainer(entityTopicNameParameters);
